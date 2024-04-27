f0dd2e917943214c013fbc4dd4714fac241a9d43
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-04-27 16:38:13 +0200

    feat(BreastCancerClassifier): created method predict to predict real life data

--- MLP-classification-breast-cancer/main.py
+++ MLP-classification-breast-cancer/main.py
@@ -45,17 +45,21 @@ class BreastCancerClassifier:
         accuracy = accuracy_score(self.Y_test, Y_pred)
         print("Accuracy:", accuracy)
 
-    def predict(self, data):
-        data = self.scaler.transform(data)
-        print(data)
-        return self.model.predict(data)
-
     def train_and_evaluate(self):
         self.load_data()
         self.prepare_data()
         self.normalize_data()
         self.train_model()
         self.evaluate_model()
+
+    def predict(self, data):
+        # Normalizza i nuovi dati
+        data_normalized = self.scaler.transform(data)
+        # Fai previsioni
+        predictions = self.model.predict(data_normalized)
+        # Decodifica le previsioni in label (M o B)
+        decoded_predictions = self.label_encoder.inverse_transform(predictions)
+        return decoded_predictions
 
     def save_model(self):
         dump(self.model, self.model_path)
@@ -65,9 +69,79 @@ class BreastCancerClassifier:
 
 
 # Utilizzo della classe
-model_path = "models\\breast_cancer_model.joblib"
-classifier = BreastCancerClassifier("data\Cancer_Data.csv", model_path=model_path)
+model_path = "MLP-classification-breast-cancer\models\\breast_cancer_model.joblib"
+classifier = BreastCancerClassifier(
+    "MLP-classification-breast-cancer\data\Cancer_Data.csv", model_path=model_path
+)
 classifier.train_and_evaluate()
 classifier.save_model()
 
-random_data = [18.61, 20.25, 122.1, 1094, 0.0944, 0.1066, 0.149, 0.07731]
+data_to_predict = [
+    [
+        17.99,
+        10.38,
+        122.8,
+        1001,
+        0.1184,
+        0.2776,
+        0.3001,
+        0.1471,
+        0.2419,
+        0.07871,
+        1.095,
+        0.9053,
+        8.589,
+        153.4,
+        0.006399,
+        0.04904,
+        0.05373,
+        0.01587,
+        0.03003,
+        0.006193,
+        25.38,
+        17.33,
+        184.6,
+        2019,
+        0.1622,
+        0.6656,
+        0.7119,
+        0.2654,
+        0.4601,
+        0.1189,
+    ],
+    [
+        13.54,
+        14.36,
+        87.46,
+        566.3,
+        0.09779,
+        0.08129,
+        0.06664,
+        0.04781,
+        0.1885,
+        0.05766,
+        0.2699,
+        0.7886,
+        2.058,
+        23.56,
+        0.008462,
+        0.0146,
+        0.02387,
+        0.01315,
+        0.0198,
+        0.0023,
+        15.11,
+        19.26,
+        99.7,
+        711.2,
+        0.144,
+        0.1773,
+        0.239,
+        0.1288,
+        0.2977,
+        0.07259,
+    ],
+]
+
+prediction = classifier.predict(data_to_predict)
+print("Prediction:", prediction)
